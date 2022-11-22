385b62f4b7df2b2cf62e167f0e8bda2218fbd420
StuartMolnar <stuartmolnar@hotmail.com>
2022-11-22 01:14:08 -0800

    split into files

---
+++ dev/data.tf
@@ -0,0 +1,14 @@
+# Set the SSH key used
+data "digitalocean_ssh_key" "my_key" {
+  name = "my_key"
+}
+
+# Set the project used
+data "digitalocean_project" "lab_project" {
+  name = "4640_labs"
+}
+
+# Create a new tag
+resource "digitalocean_tag" "do_tag" {
+  name = "Web"
+}
---
+++ dev/database.tf
@@ -0,0 +1,25 @@
+# Create a database firewall
+resource "digitalocean_database_firewall" "mongodb-firewall" {
+
+  cluster_id = digitalocean_database_cluster.mongodb-example.id
+  # allow connection from resources with a given tag
+  # for example if our droplets all have a tag "web" we could use web as the value
+  rule {
+    type  = "tag"
+    value = "web"
+  }
+}
+
+# Create a database
+resource "digitalocean_database_cluster" "mongodb-example" {
+  name       = "example-mongo-cluster"
+  engine     = "mongodb"
+  version    = "4"
+  size       = "db-s-1vcpu-1gb"
+  region     = var.region
+  node_count = 1
+
+  private_network_uuid = digitalocean_vpc.web_vpc.id
+}
+
+
