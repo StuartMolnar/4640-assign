34dde4f187c07f6c3c1679f8744c14aed1602f08
StuartMolnar <stuartmolnar@hotmail.com>
2022-11-22 21:43:38 -0800

    c

--- dev/servers.tf
+++ dev/servers.tf
@@ -1,4 +1,4 @@
-# Create firewall for droplets 
+/*# Create firewall for droplets 
 resource "digitalocean_firewall" "web" {
 
     # The name we give our firewall for ease of use                            #    
@@ -108,3 +108,4 @@ resource "digitalocean_loadbalancer" "public" {
   droplet_tag = "Web"
   vpc_uuid = digitalocean_vpc.web_vpc.id
 }
+*/
